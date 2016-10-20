f17ee4fa0e0fa399048ebf9c9888eef2b10f5772
Dana Dghaym <dd4g12@soton.ac.uk>
2016-10-20 10:31:30 +0100

    TR-Leaf3 is updated to generate type invariant in case of the refinement of the replicator child.

--- ac.soton.eventb.atomicitydecomposition.generator/src/ac/soton/eventb/atomicitydecomposition/generator/rules/TR_leaf3_rLeaf_tInv.java
+++ ac.soton.eventb.atomicitydecomposition.generator/src/ac/soton/eventb/atomicitydecomposition/generator/rules/TR_leaf3_rLeaf_tInv.java
@@ -13,6 +13,7 @@ import ac.soton.eventb.atomicitydecomposition.Leaf;
 import ac.soton.eventb.atomicitydecomposition.One;
 import ac.soton.eventb.atomicitydecomposition.Par;
 import ac.soton.eventb.atomicitydecomposition.Some;
+import ac.soton.eventb.atomicitydecomposition.TypedParameterExpression;
 import ac.soton.eventb.atomicitydecomposition.generator.strings.Strings;
 import ac.soton.eventb.atomicitydecomposition.generator.utils.Utils;
 import ac.soton.eventb.emf.diagrams.generator.AbstractRule;
@@ -27,21 +28,18 @@ public class TR_leaf3_rLeaf_tInv extends AbstractRule implements IRule {
 	public boolean enabled(EventBElement sourceElement) throws Exception  {
 		Leaf sourceLeaf = (Leaf) sourceElement;
 		
-		return 	(sourceLeaf.getDecompose().isEmpty()) && (
-				(sourceLeaf.eContainer() instanceof All) ||
-				(sourceLeaf.eContainer() instanceof Some) ||
-				(sourceLeaf.eContainer() instanceof One) ||
-				(sourceLeaf.eContainer() instanceof Par));
-		
-	/*//Dana: fixed to add type during refinement
-		 //Do it later will need to add a loop to the tranlsation or may be add a general method for this because used more than once
+	//Dana: fixed to add type during refinement
+	 
 		return sourceLeaf.getDecompose().isEmpty() &&
-				!Utils.repAncestor(sourceLeaf).isEmpty();*/
+				!Utils.repAncestorFirstChild(sourceLeaf).isEmpty();
 	}
 
 	
 	/**
 	 * TR_leaf3, Transform a replicator leaf to a typing invariant
+	 * Dana: This is updated to cover the refinement of a replicator
+	 * The only case that still needs to add additional type invariants manually 
+	 * if the first child is par that is also the child of a replicator
 	 */
 	@Override
 	public List<GenerationDescriptor> fire(EventBElement sourceElement, List<GenerationDescriptor> generatedElements) throws Exception {
@@ -63,41 +61,28 @@ public class TR_leaf3_rLeaf_tInv extends AbstractRule implements IRule {
 
 	private String generatePredicate(Leaf l) {
 		String pred = "";
-		FlowDiagram parentFLow = Utils.getParentFlow(l);
-		// SI case
+		FlowDiagram parentFlow = Utils.getParentFlow(l);
+		List<TypedParameterExpression> pars = parentFlow.getParameters();
+		if(l.eContainer() instanceof All)
+			pars.add(((All) l.eContainer()).getNewParameter());
+		else if(l.eContainer() instanceof Some)
+			pars.add(((Some) l.eContainer()).getNewParameter());
+		else if(l.eContainer() instanceof One)
+			pars.add(((One) l.eContainer()).getNewParameter());
+		else if(l.eContainer() instanceof Par)
+			pars.add(((Par) l.eContainer()).getNewParameter());
 	
-		if(parentFLow.getParameters().isEmpty()){
-			if(l.eContainer() instanceof All)
-				pred =  l.getName() + Strings.B_SUBSETEQ + ((All)l.eContainer()).getNewParameter().getType();
-			else if(l.eContainer() instanceof Some)
-				pred = l.getName() + Strings.B_SUBSETEQ + ((Some)l.eContainer()).getNewParameter().getType();
-			else if(l.eContainer() instanceof One)
-				pred = l.getName() + Strings.B_SUBSETEQ + ((One)l.eContainer()).getNewParameter().getType();
-			else if(l.eContainer() instanceof Par)
-				pred = l.getName() + Strings.B_SUBSETEQ + ((Par)l.eContainer()).getNewParameter().getType();
-		}
-		
-		//Dana fixed the MI case
-		else{
-				if(l.eContainer() instanceof All){
-					pred = l.getName() + Strings.B_SUBSETEQ + Utils.getParTypeCartesian(parentFLow.getParameters()) +
-							Strings.B_CPROD + ((All)l.eContainer()).getNewParameter().getType();
-				}
-				else if(l.eContainer() instanceof Some){
-					pred = l.getName() + Strings.B_SUBSETEQ + Utils.getParTypeCartesian(parentFLow.getParameters()) +
-							Strings.B_CPROD + ((Some)l.eContainer()).getNewParameter().getType();
-				}
-				else if(l.eContainer() instanceof One){
-					pred = l.getName() + Strings.B_SUBSETEQ + Utils.getParTypeCartesian(parentFLow.getParameters()) +
-							Strings.B_CPROD + ((One)l.eContainer()).getNewParameter().getType();
-				}
-				else if(l.eContainer() instanceof Par){
-					pred = l.getName() + Strings.B_SUBSETEQ + Utils.getParTypeCartesian(parentFLow.getParameters()) +
-							Strings.B_CPROD + ((Par)l.eContainer()).getNewParameter().getType();
-				}
-				
-			
-		}
+		// SI case
+		if(pars.size() == 1)
+			pred = l.getName() + Strings.B_SUBSETEQ + pars.get(0).getType();
+		//MI case
+		else if (pars.size() > 1)
+			pred = l.getName() + Strings.B_SUBSETEQ + Utils.getParTypeCartesian(pars);
+		else 
+			pred = "Should not reach this case";
+	
+
+
 		return pred;
 	}
 	
